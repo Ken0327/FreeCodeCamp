14d533a38fe021a530a7df16e67f8a3ec46800cc
kenChen <dr.k0327@gmail.com>
2020-03-11 09:27:33 +0800

    add basic javascript

---
+++ JavaScriptAlgorithmsNDataStructures/BasicJavascript/45_shopping-list.js
@@ -0,0 +1,9 @@
+var myList = [];
+
+myList.push(["Chocolate Bar", 15]);
+myList.push(["Chocolate", 25]);
+myList.push(["Milk", 59]);
+myList.push(["Toast", 69]);
+myList.push(["Butter", 79]);
+
+console.log(myList);
