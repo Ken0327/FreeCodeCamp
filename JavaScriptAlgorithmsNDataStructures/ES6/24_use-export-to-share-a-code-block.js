6d7459394272c616f8c3b145be7e5a104a463304
kenChen <dr.k0327@gmail.com>
2020-03-30 09:32:59 +0800

    update ES6 to 28

---
+++ JavaScriptAlgorithmsNDataStructures/ES6/24_use-export-to-share-a-code-block.js
@@ -0,0 +1,19 @@
+// method 1
+// export const uppercaseString = (string) => {
+//   return string.toUpperCase();
+// }
+
+// export const lowercaseString = (string) => {
+//   return string.toLowerCase()
+// }
+
+// method 2
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+
+export {uppercaseString, lowercaseString}
